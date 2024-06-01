5b1cd0a9972dcecb8b5ede26adfbd523d74e6a68
Aslanidis,Michael M.J.M <476238@student.fontys.nl>
2024-06-02 01:36:36 +0200

    feat/react-router, 404 design, blobity.
    Split up app.jsx to that is mainly acts as a react router, with homepage.jsx now holding the main page content

--- src/blobityConfig.js
+++ src/blobityConfig.js
@@ -1,3 +1,4 @@
+
 const initializeBlobity = () => {
     const blobity = new window.Blobity({
         licenseKey: "opensource",
@@ -7,7 +8,11 @@ const initializeBlobity = () => {
         dotColor: "#0e1016",
         invert: true,
         focusableElements: "[data-blobity], a:not([data-no-blobity]), h4:not([data-no-blobity]), li:not([data-no-blobity]), button:not([data-no-blobity]), [data-blobity-tooltip]",
+        font: "'Montserrat','Source Sans Pro',-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif",
+        fontSize: 16,
+        fontWeight: 500,
         opacity: 1,
+        fontColor: "#0e1016",
         zIndex: 100,
         size: 40,
         radius: 4,
